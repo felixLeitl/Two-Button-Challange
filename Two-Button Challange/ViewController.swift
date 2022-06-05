05d6fb32eda307cfd6b35638f939ad605ccda327
Felix Leitl <felix.leitl@icloud.com>
2022-06-05 18:30:36 +0200

    Two-Button-Alignment

--- Two-Button Challange/ViewController.swift
+++ Two-Button Challange/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func oneButtonPressed(_ sender: UIButton) {
+        messageLabel.text = "You are"
+        messageLabel.textColor = UIColor.blue
+        messageLabel.textAlignment = .left
+    }
+    
+    @IBAction func twoButtonPressed(_ sender: UIButton) {
+        messageLabel.text = "Awsome!"
+        messageLabel.textColor = UIColor.systemRed
+        messageLabel.textAlignment = .right
 
+    
+    }
 }
 
